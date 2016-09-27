a4924e36608dc497247f01578ccf410b0464765f
Kenny Williams <kennyjwilli@gmail.com>
2016-09-26 17:45:41 -0700

    Formatting

--- src/boot/new/provisdom_clj.clj
+++ src/boot/new/provisdom_clj.clj
@@ -1,28 +1,27 @@
 (ns boot.new.provisdom-clj
   "Generate a basic application project."
-  (:require [boot.new.templates :refer [renderer year date project-name
-                                        ->files sanitize-ns name-to-path
-                                        multi-segment]]))
+  (:require [boot.new.templates :as boot-new]))
 
 (defn provisdom-clj
   "A provisdom application project template."
   [name]
-  (let [render (renderer "provisdom-clj")
-        main-ns (multi-segment (sanitize-ns name))
+  (let [render (boot-new/renderer "provisdom-clj")
+        main-ns (boot-new/multi-segment (boot-new/sanitize-ns name))
         data {:raw-name    name
-              :name        (project-name name)
+              :name        (boot-new/project-name name)
               :namespace   main-ns
-              :nested-dirs (name-to-path main-ns)
-              :year        (year)
-              :date        (date)}]
+              :nested-dirs (boot-new/name-to-path main-ns)
+              :year        (boot-new/year)
+              :date        (boot-new/date)}]
     (println "Generating a project called" name "based on the 'provisdom-clj' template.")
-    (->files data
-             ["build.boot" (render "build.boot" data)]
-             ["README.md" (render "README.md" data)]
-             ["doc/intro.md" (render "intro.md" data)]
-             [".gitignore" (render "gitignore" data)]
-             ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
-             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
-             ["LICENSE" (render "LICENSE" data)]
-             ["CHANGELOG.md" (render "CHANGELOG.md" data)]
-             [".envrc" (render "envrc" data)])))
+    (boot-new/->files
+      data
+      ["build.boot" (render "build.boot" data)]
+      ["README.md" (render "README.md" data)]
+      ["doc/intro.md" (render "intro.md" data)]
+      [".gitignore" (render "gitignore" data)]
+      ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
+      ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+      ["LICENSE" (render "LICENSE" data)]
+      ["CHANGELOG.md" (render "CHANGELOG.md" data)]
+      [".envrc" (render "envrc" data)])))
