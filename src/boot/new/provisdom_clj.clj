552b5f7bc4eb3ae23cd38d9ee641bd1999a5678e
Kenny Williams <kennyjwilli@gmail.com>
2016-09-26 18:52:19 -0700

    Make the default test ns be t_core.clj

--- src/boot/new/provisdom_clj.clj
+++ src/boot/new/provisdom_clj.clj
@@ -1,16 +1,27 @@
 (ns boot.new.provisdom-clj
   "Generate a basic application project."
-  (:require [boot.new.templates :as boot-new]))
+  (:require [boot.new.templates :as boot-new]
+            [clojure.string :as str]))
+
+(defn has-core?
+  [s]
+  (= (last (str/split s #"\.")) "core"))
+
+(defn name->dirs
+  [n]
+  (as-> n $ (str/split $ #"\.") (butlast $) (str/join java.io.File/separator $) (boot-new/sanitize $)))
 
 (defn provisdom-clj
   "A provisdom application project template."
   [name]
   (let [render (boot-new/renderer "provisdom-clj")
-        main-ns (boot-new/multi-segment (boot-new/sanitize-ns name))
+        main-ns (let [n (boot-new/multi-segment (boot-new/sanitize-ns name))]
+                  (if (has-core? n) n (str n ".core")))
         data {:raw-name    name
               :name        (boot-new/project-name name)
               :namespace   main-ns
               :nested-dirs (boot-new/name-to-path main-ns)
+              :parent-dirs (name->dirs main-ns)
               :year        (boot-new/year)
               :date        (boot-new/date)}]
     (println "Generating a project called" name "based on the 'provisdom-clj' template.")
@@ -21,7 +32,7 @@
       ["doc/intro.md" (render "intro.md" data)]
       [".gitignore" (render "gitignore" data)]
       ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
-      ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+      ["test/{{parent-dirs}}/t_core.clj" (render "test.clj" data)]
       ["LICENSE" (render "LICENSE" data)]
       ["CHANGELOG.md" (render "CHANGELOG.md" data)]
       [".envrc" (render "envrc" data)]
